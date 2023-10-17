1cd5dc618a16229fb772eeb07b77255d7bfa3c67
Nisha01jangid <nishajangid629@gmail.com>
2023-10-17 09:18:02 +0530

    Barcode working is done

--- app.js
+++ app.js
@@ -7,31 +7,70 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Sample product data (replace with your data source)
 const products = {
-  '12345': { name: 'Product 1', description: 'Description 1' },
-  '67890': { name: 'Product 2', description: 'Description 2' },
+  'spG146558127': 
+  { 
+    name: 'spG146558127',
+    title: 'Medusa Coffee Mug', 
+    subtitle:null,
+    description:"Every programmer's best friend.",
+    handle:"coffee-mug",
+    is_giftcard:false,
+    status:"published",
+    thumbnail:"https://medusa-public-images.s3.eu-west-1.amazonaws.com/coffee-mug.png",
+    weight:400,
+    length:300,
+    height:500,
+    width:150,
+    hs_code:'medusa_hs_code_for_test',
+    origin_country:'India',
+    mid_code:12343454324234,
+    material:'cotton',
+    type_id:1231234,
+    discountable:true,
+    external_id:null,
+    metadata:'Best Products ever on discount',
+    collection:'Star collections',
+    collection_id:2313412312
+
+  },
+
 };
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/scan', async (req, res) => {
+app.post('/scan', (req, res) => {
   const barcode = req.body.barcode;
-
-  try {
-    // Make an API request to fetch product details
-    const response = await axios.get(`http://localhost:9000/products/${barcode}`);
-    const product = response.data;
-
+  const product = products[barcode];
+  if (product) {
     res.render('product', { product });
-  } catch (error) {
+  } else {
     res.render('not-found');
   }
 });
 
+
+// app.post('/scan', async (req, res) => {
+//   const barcode = req.body.barcode;
+
+//   try {
+//     // Make an API request to fetch product details
+//     const response = await axios.get(`http://localhost:9000/store/products/${barcode}`);
+//     const productData = response.data;
+//     console.log(productData)
+
+//     res.render('product', {product: productData.product });
+//   } catch (error) {
+//     res.render('not-found');
+//   }
+// });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
